1edd1a760d321949cb51f1f6572954d5c46a1201
Liara Graminho Victor <liaragraminhovictor@gmail.com>
2025-07-10 22:17:49 -0300

    .

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,23 +1,21 @@
-/** biome-ignore-all assist/source/organizeImports: test */
-/** biome-ignore-all lint/correctness/noUnusedImports: test */
-import { Routes, Route, BrowserRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CreateRoom } from "./pages/create-room"
-import { Room } from "./pages/room"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
 
 const queryClient = new QueryClient()
 
-function App() {
+export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room/:roomId"/>
+          <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  ) 
+  )
 }
-
-export default App
